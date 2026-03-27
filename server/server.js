003fab793dcfb47f56d1e1f9e2b24fc8ca6672ed
Egnaldo Dias <seu-email@exemplo.com>
2026-03-27 20:49:20 -0300

    corrigir path duplicado

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 app.use(express.static(path.join(__dirname, '..')));
 const cors = require('cors');
 const { GoogleAuth } = require('google-auth-library');
